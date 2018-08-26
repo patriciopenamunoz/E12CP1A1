240c0d691a92b0888eb817948246203688d6ce3f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-08-26 19:26:13 -0300

    Preparar vendor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,14 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ico')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
